30d7be87e78eca59a59b7bedccea2392eb0b984b
Dorothee <braud.dorothee@gmail.com>
2024-10-17 11:17:29 +0200

    /Affiche le message dans ta console. Tu devrais avoir le message Bonjour qui apparaît.

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 //ETAPE2 : Crée une variable message dans lequel on va stocker le message : Bonjour !
 let message1 = "Bonjour !"
+
+//Affiche le message dans ta console. Tu devrais avoir le message “Bonjour !” qui apparaît.
+console.log(message1)
